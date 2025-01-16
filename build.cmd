a528bd60c0428329269db76745497f6ac340ff5f
skywind3000 <skywind3000@163.com>
2025-01-16 17:21:28 +0800

    building script

---
+++ build.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+set "SCRIPT_HOME=%~dp0"
+cd /D "%SCRIPT_HOME%"
+
+set GOOS=linux
+set GOARCH=amd64
+set CGO_ENABLED=0
+echo Building for linux/amd64
+go build -o bin\uboost-linux-amd64 .
+
+set GOOS=windows
+set GOARCH=amd64
+set CGO_ENABLED=0
+echo Building for windows/amd64
+go build -o bin\uboost-windows-amd64 .
+
+
